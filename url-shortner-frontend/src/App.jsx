b7ab7663cc4846017dfb05cd04869c42edb6b42e
Anish Kalbhor <anishkalbhor2020@gmail.com>
2025-08-27 17:42:16 +0530

    feat: add RegisterPage component with form handling and validation, integrate axios for API calls, and configure CORS in backend

--- url-shortner-frontend/src/App.jsx
+++ url-shortner-frontend/src/App.jsx
@@ -4,15 +4,19 @@ import LandingPage from "./components/LandingPage";
 import AboutPage from "./components/AboutPage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import RegisterPage from "./components/RegisterPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <Router>
       <Navbar />
+      <Toaster position="bottom-center" />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       <Footer />
       </Router>
