2ccfabd5f6a53de79a2434a5343cabe8dfadc2ad
Anish Kalbhor <anishkalbhor2020@gmail.com>
2025-08-27 23:29:44 +0530

    feat: add dashboard layout and graph components, implement login functionality, and include dummy data for visualization

--- url-shortner-frontend/src/App.jsx
+++ url-shortner-frontend/src/App.jsx
@@ -6,7 +6,8 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import RegisterPage from "./components/RegisterPage";
 import { Toaster } from "react-hot-toast";
-import Login from "./components/Login";
+import LoginPage from "./components/LoginPage";
+import DashboardLayout from "./Dashboard/DashboardLayout";
 
 function App() {
   return (
@@ -18,7 +19,8 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardLayout />} />
         </Routes>
       <Footer />
       </Router>
