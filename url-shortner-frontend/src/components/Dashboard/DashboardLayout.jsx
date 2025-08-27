d29db3ff49a5fcf59ef633749576ee3f1142988b
Anish Kalbhor <anishkalbhor2020@gmail.com>
2025-08-28 01:34:39 +0530

    feat: Add Create New Short URL functionality and Dashboard components
    - Updated package.json to include MUI and Emotion dependencies.
    - Refactored App component to import DashboardLayout from the correct path.
    - Created CreateNewShorten component for URL shortening functionality.
    - Added DashboardLayout component to display graphs and manage URL shortening.
    - Implemented Graph component to visualize total clicks data.
    - Added ShortenPopUp component to handle modal for creating new short URLs.

--- url-shortner-frontend/src/components/Dashboard/DashboardLayout.jsx
+++ url-shortner-frontend/src/components/Dashboard/DashboardLayout.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Graph from "./Graph";
-import { useStoreContext } from "../contextApi/ContextApi";
-import { useFetchTotalClicks } from "../hooks/useQuery";
+import { useStoreContext } from "../../contextApi/ContextApi";
+import { useFetchTotalClicks } from "../../hooks/useQuery";
+import ShortenPopUp from "./ShortenPopUp";
 
 const DashboardLayout = () => {
+  const refetch = false;
   const { token } = useStoreContext();
+  const [shortenPopUp, setShortenPopUp] = useState(false);
 
   // console.log(useFetchTotalClicks(token, onError));
 
@@ -39,12 +42,20 @@ const DashboardLayout = () => {
             <Graph graphData={totalClicks} />
           </div>
           <div className="py-5 sm:text-end text-center">
-            <button className="bg-custom-gradient px-4 py-2 rounded-md text-white">
+            <button className="bg-custom-gradient px-4 py-2 rounded-md text-white"
+            onClick={() => setShortenPopUp(true)}
+            >
               Create a New Short URL
             </button>
           </div>
         </div>
       )}
+
+      <ShortenPopUp 
+      refetch={refetch}
+      open={shortenPopUp}
+      setOpen={setShortenPopUp}
+      />
     </div>
   );
 };
