99599fc1afa4a0ae16ede4f4da579fc4847d6b40
Anish Kalbhor <anishkalbhor2020@gmail.com>
2025-08-27 16:31:28 +0530

    feat: add AboutPage, Card, Footer, LandingPage, and Navbar components with responsive design and animations

--- url-shortner-frontend/src/App.jsx
+++ url-shortner-frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import AboutPage from "./components/AboutPage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <>
-      <h2 class="text-3xl font-bold underline">
-    Hello world!
-  </h2>
+      <Router>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
+      <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
